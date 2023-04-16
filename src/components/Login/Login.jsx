cd810ff486cdf58dab80314df5e7dc344784d7ee
chayansrk64 <chayansrk64@gmail.com>
2023-04-17 00:10:54 +0600

    login page done

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,8 @@
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import React, { useState } from "react";
+import app from "../../firebase/firebase-config";
 
-
+const auth = getAuth(app);
 
 const Login = () => {
 
@@ -29,9 +31,22 @@ const Login = () => {
             setError('Password length must be 6 in number')
             return;
          }
+
+         createUserWithEmailAndPassword(auth, email, password)
+         .then(result => {
+          const user = result.user;
+          console.log(user);
+          event.target.reset();
+          setSuccess('User Logged in Succesfully')
+         })
+         .catch(error => {
+          console.error(error);
+          setError(error.message)
+         })
     }
   return (
     <div>
+      <h2 className="text-center">Log In</h2>
       <form onSubmit={handleSubmit} className="mx-auto w-25">
         <div className="form-group">
           <label htmlFor="email">Email address</label>
@@ -60,7 +75,7 @@ const Login = () => {
         </button>
       </form>
       <div ><p className="text-danger text-center ">{error}</p></div>
-      <div ><p className="text-danger text-center ">{success}</p></div>
+      <div ><p className="text-center text-success ">{success}</p></div>
     </div>
   );
 };
